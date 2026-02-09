858bec89cce8fb8874cf5fbdfa8f84848838ae16
rhest012 <hola@rhest.studio>
2026-02-09 07:13:16 +0200

    Nasty Launch

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,10 @@ export default function Home() {
       />
       <div className="relative p-8 !z-10 w-full h-full flex flex-col w-full justify-center items-center mx-0 lg:px-50">
         <Image src={NastyLogo} alt="Nasty Logo" height={100} className="mb-4" />
-        <p></p>
+        <p>
+          We cordially invite you to join us for the official launch of Nasty
+          ENRG in South Africa.
+        </p>
         <RsvpForm />
       </div>
     </div>
